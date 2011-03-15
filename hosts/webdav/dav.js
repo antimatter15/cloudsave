79a85b05f419fabfe032da7a4af7ba16182ff041
antimatter15 <antimatter15@gmail.com>
2011-03-14 23:08:36 -0400

    blah

---
+++ hosts/webdav/dav.js
@@ -0,0 +1,65 @@
+//does not use multipart helper function because its not multipart.
+
+
+Hosts.WebDAV = function uploadWebDAV(req, callback){
+  if(!localStorage.webdav_url){
+		localStorage.webdav_url = prompt("Enter the URL of the WebDAV provider");
+	}
+	if(!localStorage.webdav_url || !localStorage.webdav_url.test(/^http/)){
+		return callback("error: invalid webdav server url");
+	}
+
+	var fs = new WebDAV.Fs(localStorage.webdav_url);
+
+	getRaw(req, function(file){
+		var body = new BlobBuilder();
+		var bin = file.data, arr = new Uint8Array(bin.length);
+		for(var i = 0; i < bin.length; i++){
+			arr[i] = bin.charCodeAt(i);
+		}
+		body.append(arr.buffer);
+		fs.file(file.name).write(body.getBlob(), function(body){
+			console.log(body);
+		});
+	});
+	/*
+  var poll = function(){
+    if(dropbox.isAccessGranted()){
+      getRaw(req, function(file){
+        var fname =  file.name;
+        var folder = ''
+        
+        dropbox.getAccountInfo(function(user){
+        
+        
+        dropbox.getDirectoryMetadata(folder + encodeURIComponent(file.name), function(json){
+          if(json.error && json.error.indexOf('not found') != -1){
+            //yay plop it on the top
+          }else if(fname.indexOf('/') == -1){
+            fname = Math.random().toString(36).substr(2,4) + '_' + fname;
+          }else{
+            //no idea. TODO: do something
+          }
+          
+          
+        dropbox.putFileContents(folder + fname, file,
+          function(){
+            console.log('done uploading');
+            //yay done. hopefully
+              console.log('got stuffs now');
+              callback()
+          });
+        })
+          
+          })
+          
+      })
+    }else{
+      setTimeout(poll, 300);
+    }
+  };
+  poll();
+  
+  */
+	//yay
+}
---
+++ hosts/webdav/webdav.js
@@ -0,0 +1,147 @@
+// A raw WebDAV interface
+var WebDAV = {
+  GET: function(url, callback) {
+    return this.request('GET', url, {}, null, 'text', callback);
+  },
+
+  PROPFIND: function(url, callback) {
+    return this.request('PROPFIND', url, {Depth: "1"}, null, 'xml', callback);
+  },
+
+  MKCOL: function(url, callback) {
+    return this.request('MKCOL', url, {}, null, 'text', callback);
+  },
+  
+  DELETE: function(url, callback) {
+    return this.request('DELETE', url, {}, null, 'text', callback);
+  },
+
+  PUT: function(url, data, callback) {
+    return this.request('PUT', url, {}, data, 'text', callback);
+  },
+  
+  request: function(verb, url, headers, data, type, callback) {
+    var xhr = new XMLHttpRequest();
+    var body = function() {
+      var b = xhr.responseText;
+      if (type == 'xml') {
+        var xml = xhr.responseXML;
+        if(xml) {
+          b = xml.firstChild.nextSibling ? xml.firstChild.nextSibling : xml.firstChild;
+        }
+      }
+      return b;
+    };
+    
+    if(callback) {
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) { // complete.
+          var b = body();
+          if(b) {
+            callback(b);
+          }
+        }
+      };
+    }
+    xhr.open(verb, url, !!callback);
+    xhr.setRequestHeader("Content-Type", "text/xml; charset=UTF-8");
+    for (var header in headers) {
+      xhr.setRequestHeader(header, headers[header]);
+    }
+    xhr.send(data);
+
+    if(!callback) {
+      return body();
+    }
+  }
+};
+
+// An Object-oriented API around WebDAV.
+WebDAV.Fs = function(rootUrl) {
+  this.rootUrl = rootUrl;
+  var fs = this;
+  
+  this.file = function(href) {
+    this.type = 'file';
+
+    this.url = fs.urlFor(href);
+
+    this.name = fs.nameFor(this.url);
+
+    this.read = function(callback) {
+      return WebDAV.GET(this.url, callback);
+    };
+
+    this.write = function(data, callback) {
+      return WebDAV.PUT(this.url, data, callback);
+    };
+
+    this.rm = function(callback) {
+      return WebDAV.DELETE(this.url, callback);
+    };
+
+    return this;
+  };
+  
+  this.dir = function(href) {
+    this.type = 'dir';
+
+    this.url = fs.urlFor(href);
+
+    this.name = fs.nameFor(this.url);
+
+    this.children = function(callback) {
+      var childrenFunc = function(doc) {
+        if(doc.childNodes == null) {
+          throw('No such directory: ' + url);
+        }
+        var result = [];
+        // Start at 1, because the 0th is the same as self.
+        for(var i=1; i< doc.childNodes.length; i++) {
+          var response     = doc.childNodes[i];
+          var href         = response.getElementsByTagName('D:href')[0].firstChild.nodeValue;
+          href = href.replace(/\/$/, ''); // Strip trailing slash
+          var propstat     = response.getElementsByTagName('D:propstat')[0];
+          var prop         = propstat.getElementsByTagName('D:prop')[0];
+          var resourcetype = prop.getElementsByTagName('D:resourcetype')[0];
+          var collection   = resourcetype.getElementsByTagName('D:collection')[0];
+
+          if(collection) {
+            result[i-1] = new fs.dir(href);
+          } else {
+            result[i-1] = new fs.file(href);
+          }
+        }
+        return result;
+      };
+
+      if(callback) {
+        WebDAV.PROPFIND(this.url, function(doc) {
+          callback(childrenFunc(doc));
+        });
+      } else {
+        return childrenFunc(WebDAV.PROPFIND(this.url));
+      }
+    };
+
+    this.rm = function(callback) {
+      return WebDAV.DELETE(this.url, callback);
+    };
+
+    this.mkdir = function(callback) {
+      return WebDAV.MKCOL(this.url, callback);
+    };
+
+    return this;
+  };
+  
+  this.urlFor = function(href) {
+    return (/^http/.test(href) ? href : this.rootUrl + href);
+  };
+  
+  this.nameFor = function(url) {
+    return url.replace(/.*\/(.*)/, '$1');
+  };
+
+  return this;
+};
